ecbc8c0aa195642a7c353caa336e8f4d5a57936a
Zuki Air <zukirust@gmail.com>
2024-11-30 18:01:01 +0000

    add workaround for screenlocker crashes

--- hypr/hyprland_lock.sh
+++ hypr/hyprland_lock.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
-swaylock -i ~/.config/wallpaper
+hyprland_lock="swaylock -i ~/.config/wallpaper"
+sessionlocked=1
+while [ "$sessionlocked" == "1" ] && ! [ "$sessiontimeout" == "100" ]; do 
+$hyprland_lock && sessionlocked=0
+#sessiontimeout=$(($sessiontimeout + 1))
+#echo "$sessiontimeout" 
+done
