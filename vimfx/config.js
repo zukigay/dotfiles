226701b1b2b6314f956ab1c4b034944637d3fe97
Zuki Air <zukirust@gmail.com>
2024-12-16 15:54:49 +0000

    vimfx: add comment explaing blacklist

--- vimfx/config.js
+++ vimfx/config.js
@@ -5,6 +5,7 @@
 // source https://github.com/lydell/dotfiles/blob/1213fc092006586164ad777203958aa55b94c62d/.vimfx/config.js#L8
 const {Preferences} = Cu.import('resource://gre/modules/Preferences.jsm', {}
 
+// blacklist vimfx from running on neflix and google docs since they don't play nice with vimfx
 vimfx.set('blacklist', '*www.netflix.com* *docs.google.com*')
 
 // source https://github.com/lydell/dotfiles/blob/1213fc092006586164ad777203958aa55b94c62d/.vimfx/config.js#L63
