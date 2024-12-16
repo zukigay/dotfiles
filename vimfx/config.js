a96c97a8195cdf55cd96451e0c0be6f797dc3d3f
Zuki Air <zukirust@gmail.com>
2024-12-16 17:17:25 +0000

    vimfx: switch from blocklist to keyoverride for netflix

--- vimfx/config.js
+++ vimfx/config.js
@@ -9,7 +9,8 @@ const {Preferences} = Cu.import('resource://gre/modules/Preferences.jsm', {})
 console.log("loading zuki's vimfx config :", vimfx)
 
 // blacklist vimfx from running on neflix and google docs since they don't play nice with vimfx
-vimfx.set('blacklist', '*www.netflix.com* *docs.google.com*')
+//vimfx.set('blacklist', '*www.netflix.com* *docs.google.com*')
+vimfx.set('blacklist', '*docs.google.com*')
 
 // source https://github.com/lydell/dotfiles/blob/1213fc092006586164ad777203958aa55b94c62d/.vimfx/config.js#L63
 // Set firefox prefs, note this is a array.
@@ -18,3 +19,16 @@ const FIREFOX_PREFS = {
 }
 
 Preferences.set(FIREFOX_PREFS) // apply firefox prefs
+
+vimfx.addKeyOverrides(
+  [ location => location.hostname === 'keyboard-test.space',
+    ['<space>','j']
+  ]
+)
+// override a few video sites to passthough space+f rather then pass it to vimfx
+vimfx.addKeyOverrides(
+  [ location => location.hostname === 'www.netflix.com' || 'www.youtube.com',
+    ['<space>','f']
+  ]
+)
+
