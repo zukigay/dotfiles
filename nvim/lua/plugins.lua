b340db7a9722899e90bb658b3d20f19ea0e07ab7
Zuki Air <zukirust@gmail.com>
2024-12-28 11:48:21 +0000

    nvim: add nvim-telescope

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -14,7 +14,15 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
     "tanvirtin/monokai.nvim",
     "https://github.com/gentoo/gentoo-syntax",
+    "nvim-telescope/telescope.nvim", tag='0.1.8'
 })
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
 
 -- require("lazy").setup({
     -- LSP manager
