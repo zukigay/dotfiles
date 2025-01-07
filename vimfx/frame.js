49f07bc7a6d13e0f9b1c39b518a62c7ffd5000aa
Zuki Air <zukirust@gmail.com>
2025-01-07 13:20:49 +0000

    vimfx: add binds for opening active window in mpv or opening focused link in mpv

--- vimfx/frame.js
+++ vimfx/frame.js
@@ -2,3 +2,14 @@
 // this single line sets the app title of firefox to
 // {url} — Mozilla Firefox
 //content.document.title = content.document.location 
+vimfx.listen('getCurrentUrl', (data, callback) => {
+  //let {href} = content.document.activeElement
+  let {href} = content.document.location
+  callback(href)
+})
+
+vimfx.listen('getCurrentHref', (data, callback) => {
+  let {href} = content.document.activeElement
+  //let {href} = content.document.location
+  callback(href)
+})
