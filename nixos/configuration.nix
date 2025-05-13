2a8df5a645382f8c4914c1efce15369c8b2575c2
Zuki Air <zukirust@gmail.com>
2025-05-13 21:20:04 +0000

    nixos: add kakoune

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -142,6 +142,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    kakoune
     wget
     git
   ];
