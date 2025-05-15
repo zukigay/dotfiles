02e945c0e76c700a3ecdf4ec4b924c37beee8414
Zuki Air <zukirust@gmail.com>
2025-05-15 18:23:37 +0200

    nixos-wayenv: add xdg_sesson_type

--- nixos/mod/wayenv.nix
+++ nixos/mod/wayenv.nix
@@ -8,6 +8,8 @@
         MOZ_ENABLE_WAYLAND="1";
         NVD_BACKEND="direct";
         ELECTRON_OZONE_PLATFORM_HINT="auto";
+        XDG_CURRENT_DESKTOP="dwl";
+        XDG_SESSION_TYPE="wayland";
     };
  };
 }
