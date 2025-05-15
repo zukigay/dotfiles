aebbe9684cdbcd00fca73773ce04b218ce40e286
Zuki Air <zukirust@gmail.com>
2025-05-15 18:22:03 +0200

    dwl: add autostart config

--- nixos/src/dwl/config.h
+++ nixos/src/dwl/config.h
@@ -23,6 +23,18 @@ static const float fullscreen_bg[]         = {0.1f, 0.1f, 0.1f, 1.0f}; /* You ca
 /* logging */
 static int log_level = WLR_ERROR;
 
+/* Autostart */
+static const char *const autostart[] = {
+        // "wbg", "/path/to/your/image", NULL,
+        "systemctl", "--user", "import-environment", "WAYLAND_DISPLAY", "XDG_CURRENT_DESKTOP", NULL,
+        "dbus-update-activation-environment", "--systemd", "WAYLAND_DISPLAY", "XDG_CURRENT_DESKTOP=dwl", NULL,
+        "dwl-startup", NULL,
+        "hyprpaper", NULL,
+        "kanshi", NULL,
+        // "sh", "-c", "systemctl", "--user", "import-environment", "WAYLAND_DISPLAY", "XDG_CURRENT_DESKTOP", NULL
+        NULL /* terminate */
+};
+
 /* NOTE: ALWAYS keep a rule declared even if you don't use rules (e.g leave at least one example) */
 static const Rule rules[] = {
 	/* app_id             title       tags mask     isfloating   monitor */
