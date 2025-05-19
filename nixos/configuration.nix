2ff7b9b102d223ff72b7e93a586aa0d2706b5a09
Zuki Air <zukirust@gmail.com>
2025-05-19 13:35:22 +0100

    nixos: add nvidiapersistenced just in case it helps i guess

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -144,6 +144,7 @@ in
   };
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia.open = true; # Set to false to use the proprietary kernel module
+  hardware.nvidia.nvidiaPersistenced = true;
 
 
   
