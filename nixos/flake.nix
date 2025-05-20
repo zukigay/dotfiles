4e53d67e617a471dcd42ab2cd15d372bd14ef939
Zuki Air <zukirust@gmail.com>
2025-05-20 21:12:15 +0100

    nixos: switch to unstable

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -2,29 +2,38 @@
   description = "A very basic flake";
 
   inputs = {
-    # nixpkgs.url = "nixpkgs/nixos-unstable";
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
-    unstable.url = "nixpkgs/nixos-unstable";
-    qtileflake = {
-        # url = "github:qtile/qtile";
-        url = "github:qtile/qtile/v0.31.0";
-        # inputs.nixpkgs.follows = "nixpkgs";
-        inputs.nixpkgs.follows = "unstable";
-    };
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    # nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
+    # unstable.url = "nixpkgs/nixos-unstable";
+    # qtileflake = {
+    #     # url = "github:qtile/qtile";
+    #     url = "github:qtile/qtile/v0.31.0";
+    #     # inputs.nixpkgs.follows = "nixpkgs";
+    #     inputs.nixpkgs.follows = "unstable";
+    # };
+    # qtileflake = {
+       # url = "github:qtile/qtile";
+       # inputs.nixpkgs.follows = "nixpkgs";
+       #  url = "github:qtile/qtile/v0.31.0";
+        # url = "github:jwijenbergh/qtile/wayc";
+        # inputs.nixpkgs.follows = "unstable";
+     # };
+
     # nix-search = {
     #     url = "github:diamondburned/nix-search";
     #     inputs.nixpkgs.follows = "nixpkgs";
     # };
   };
 
-  outputs = { self, nixpkgs, unstable, qtileflake, ... }: {
-  # outputs = { self, nixpkgs, qtileflake, ... }: {
+  outputs = { self, nixpkgs, ... }: {
+  # outputs = { self, nixpkgs, unstable, ... }: {
+  # outputs = { self, nixpkgs, unstable, qtileflake, ... }: {
     nixosConfigurations.zuki = nixpkgs.lib.nixosSystem {
         specialArgs = { 
-            inherit unstable;
-            inherit qtileflake;
+            # inherit unstable;
         };
         modules = [
+        # (_: { nixpkgs.overlays = [ qtileflake.overlays.default ]; })
         ./configuration.nix
         ];
     };
