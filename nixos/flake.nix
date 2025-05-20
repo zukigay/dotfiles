b17619ef15ec65b6257bf4afc909083d27997183
Zuki Air <zukirust@gmail.com>
2025-05-20 15:34:38 +0100

    flake.nix: add unstable + qtileflake to inputs

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -2,15 +2,28 @@
   description = "A very basic flake";
 
   inputs = {
+    # nixpkgs.url = "nixpkgs/nixos-unstable";
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
-    nix-search = {
-        url = "github:diamondburned/nix-search";
-        inputs.nixpkgs.follows = "nixpkgs";
+    unstable.url = "nixpkgs/nixos-unstable";
+    qtileflake = {
+        # url = "github:qtile/qtile";
+        url = "github:qtile/qtile/v0.31.0";
+        # inputs.nixpkgs.follows = "nixpkgs";
+        inputs.nixpkgs.follows = "unstable";
     };
+    # nix-search = {
+    #     url = "github:diamondburned/nix-search";
+    #     inputs.nixpkgs.follows = "nixpkgs";
+    # };
   };
 
-  outputs = { self, nixpkgs, ... }: {
+  outputs = { self, nixpkgs, unstable, qtileflake, ... }: {
+  # outputs = { self, nixpkgs, qtileflake, ... }: {
     nixosConfigurations.zuki = nixpkgs.lib.nixosSystem {
+        specialArgs = { 
+            inherit unstable;
+            inherit qtileflake;
+        };
         modules = [
         ./configuration.nix
         ];
