d9577ca5a8cfc1fa94dcb5ea7e3cb2c26236a850
Zuki Air <zukirust@gmail.com>
2025-05-21 20:50:30 +0100

    nixos: add hypr-dark plugin package

---
+++ nixos/pkgs/hypr-dark/default.nix
@@ -0,0 +1,36 @@
+{pkgs,lib}:
+{
+  Hypr-DarkWindow = pkgs.gcc14Stdenv.mkDerivation {
+    pname = "Hypr-DarkWindow";
+    version = "3.0.1";
+    src = pkgs.fetchFromGitHub {
+        owner = "micha4w";
+        repo = "Hypr-DarkWindow";
+        rev = "9d541dbd1e96d1dab887d9b6f10cbefd6476d104";
+        hash = "sha256-IFfMnkQZeWuRrMxbIDfVAcbAhWmLqMSno64+NnXMIAE=";
+    };
+    # src = nix-filter.lib {
+    #   root = ./.;
+    #   include = [
+    #     "src"
+    #     ./Makefile
+    #   ];
+    # };
+
+    nativeBuildInputs = with pkgs; [ pkg-config ];
+    buildInputs = [pkgs.hyprland] ++ pkgs.hyprland.buildInputs;
+
+    installPhase = ''
+      mkdir -p $out/lib
+      install ./out/hypr-darkwindow.so $out/lib/libHypr-DarkWindow.so
+    '';
+
+    meta = with pkgs.lib; {
+      homepage = "https://github.com/micha4w/Hypr-DarkWindow";
+      description = "Invert the colors of specific Windows";
+      license = licenses.mit;
+      platforms = platforms.linux;
+    };
+  # default = Hypr-DarkWindow;
+  };
+}
