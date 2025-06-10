a82ef660465091a625269e275c9cd3ba4167d464
Zuki Air <zukirust@gmail.com>
2025-06-10 22:11:52 +0100

    nixos: update steam.nix to add protonup-qt and proton-ge-bin

--- nixos/mod/steam.nix
+++ nixos/mod/steam.nix
@@ -8,6 +8,7 @@
     environment.systemPackages = with pkgs; [
         # only really use mangohud with steam so might as well put this here.
         mangohud
+        protonup-qt
     ];
     programs.steam = {
         enable = true;
@@ -20,6 +21,14 @@
     adwaita-icon-theme
     # wine
     # wine64
+
+    # enable these if using boxtron or the scummvm version of boxtron
+    #libnotify
+    #dosbox
+    #scummvm
+    ];
+    programs.steam.extraCompatPackages = with pkgs; [
+    proton-ge-bin
     ];
  };
 
